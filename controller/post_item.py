928ac7edc799df5c96bd3816731550d5caa2743e
bellothornus2 <divanov@cifpfbmoll.eu>
2021-03-24 19:53:28 +0100

    refactor: :recycle: we changed the GET Verb of add item to POST verb

--- controller/post_item.py
+++ controller/post_item.py
@@ -1,10 +1,14 @@
-from flask import redirect, jsonify, Blueprint
+from flask import redirect, Blueprint, request
 from services import post_items
 
 post_item_blue = Blueprint("post_item", __name__)
 
 
-@post_item_blue.route("/add/item/<name>/<sell_in>/<quality>", methods=["GET"])
-def post_item(name, sell_in, quality):
+@post_item_blue.route("/item", methods=["POST"])
+def post_item():
+    data = request.get_json()
+    name = data["name"]
+    sell_in = data["sell_in"]
+    quality = data["quality"]
     post_items.post_item(name, int(sell_in), int(quality))
     return redirect("/")
