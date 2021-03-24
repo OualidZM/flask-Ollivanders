4002b77d2606be4bc2144d880f61695b569b57ae
bellothornus2 <divanov@cifpfbmoll.eu>
2021-03-24 20:34:18 +0100

    feat: :white_check_mark: all tests checked and coverage html 100% tested and covered

--- test/test_get_item.py
+++ test/test_get_item.py
@@ -7,3 +7,9 @@ def test_get_aged_brie():
     test = app.test_client()
     response = test.get("/item/Aged Brie")
     assert b'{"name":"Aged Brie"' in response.data
+
+@pytest.mark.get_item_fail
+def test_get_aged_brie_fail():
+    test = app.test_client()
+    response = test.get("/item/Aged Brie2")
+    assert b'{"none":"null"' in response.data
--- test/test_post_item.py
+++ test/test_post_item.py
@@ -5,7 +5,7 @@ from app import app
 @pytest.mark.post_item
 def test_add_item():
     test = app.test_client()
-    result = test.post("/item", json={"name":"hola", "sell_in":2, "quality":3})
+    result = test.post("/item", json={"name": "hola", "sell_in": 2, "quality": 3})
     response = test.get("/")
     assert b'{"name":"hola","quality":3,"sell_in":2}' in response.data
     assert result.status_code == 302
