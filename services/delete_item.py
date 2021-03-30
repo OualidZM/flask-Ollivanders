26f15a3bd3788b3736157bc40021f84444c69ea6
bellothornus2 <divanov@cifpfbmoll.eu>
2021-03-30 19:24:57 +0200

    feat: :sparkles: delete item with the new database created and checked but not tested

--- services/delete_item.py
+++ services/delete_item.py
@@ -1,5 +1,8 @@
-from repository import BBDD
-
+from repository.sqlite_db import Database
 
 def delete_item(name, sell_in, quality):
-    BBDD.BBDD.inventory.remove([name, sell_in, quality])
+    items = Database('Ollivanders.sqlite')
+    sql = f"delete from Item where name='{name}' and sell_in='{sell_in}' and quality='{quality}'"
+    items.execute(sql)
+    items.commit()
+    items.close()
