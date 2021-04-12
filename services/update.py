7dfc24f0fec94235c5a8b8b6d98033072692136e
Oualid Zaaj Meziani <72554668+OualidZM@users.noreply.github.com>
2021-04-13 00:06:16 +0200

    Update update.py

--- services/update.py
+++ services/update.py
@@ -1,16 +1,12 @@
 from repository.sqlite_db import item_manager, Database, Item
-# from objects_temp.Updateable import update_quality
-# from repository import BBDD
 
 def update_item(name,sell_in,quality,new_sell_in,new_quality):
     db = Database('Ollivanders.sqlite')
     Item.db = db
-    k = Item(name, sell_in, quality)
-    zz = k.name
-    if zz == name:
+    table = Item(name, sell_in, quality)
+     names = table.name
+    if names == name:
         sql = f"UPDATE Item SET sell_in = '{new_sell_in}', quality='{new_quality}' WHERE name ='{name}'"
         db.execute(sql)
         db.commit()
         db.close()
-
-update_item("test_test", 55, 100, 9999999, 141413)
