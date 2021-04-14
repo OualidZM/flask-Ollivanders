5243c5fd3efa247862f80c4c60cf53b6d7c0b5c4
bellothornus2 <divanov@cifpfbmoll.eu>
2021-04-14 17:24:49 +0200

    fix: :white_check_mark: all tests passed

--- services/get_item.py
+++ services/get_item.py
@@ -15,6 +15,15 @@ from repository import sqlite_db as DB
 #     return answer
 def get_item(item):
     manager = DB.item_manager()
-    item_db = manager.get(id=item)
-    answer = {item_db.id:{"name":item_db.name, "quality":item_db.quality, "sell_in":item_db.sell_in}}
+    try:
+        item_db = manager.get(id=item)
+        answer = {
+            item_db.id: {
+                "name": item_db.name,
+                "quality": item_db.quality,
+                "sell_in": item_db.sell_in,
+            }
+        }
+    except ValueError:
+        answer = {"none": "null"}
     return answer
--- test/test_get_item.py
+++ test/test_get_item.py
@@ -5,12 +5,12 @@ from app import app
 @pytest.mark.get_item
 def test_get_aged_brie():
     test = app.test_client()
-    response = test.get("/item/Aged Brie")
+    response = test.get("/item/320")
     assert b'{"name":"Aged Brie"' in response.data
 
 
 @pytest.mark.get_item_fail
 def test_get_aged_brie_fail():
     test = app.test_client()
-    response = test.get("/item/Aged Brie2")
+    response = test.get("/item/1")
     assert b'{"none":"null"' in response.data
